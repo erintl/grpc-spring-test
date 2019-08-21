90f34bb9ef0fa900a75cef6227ff6d317c6a259b
Tim Erin <erintl@upmc.edu>
2019-08-21 15:59:45 -0400

    Added client and server ssl

--- grpc-spring-test-client/src/main/java/com/github/erintl/grpcspringtestclient/CacheClient.java
+++ grpc-spring-test-client/src/main/java/com/github/erintl/grpcspringtestclient/CacheClient.java
@@ -1,16 +1,21 @@
 package com.github.erintl.grpcspringtestclient;
 
+import com.github.erintl.cache.CacheGrpc;
 import com.github.erintl.cache.Item;
 import com.github.erintl.cache.ItemReply;
 import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import com.github.erintl.cache.CacheGrpc;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.net.ssl.SSLException;
+import java.io.File;
+import java.util.logging.Level;
 
 @Log
 @Component
@@ -20,6 +25,8 @@ public class CacheClient
     private String host;
     @Value("${grpc.port}")
     private int port;
+    @Value("${grpc.ca-certificate}")
+    private String caCertificateName;
 
     private ManagedChannel channel;
     private CacheGrpc.CacheBlockingStub cacheStub;
@@ -28,8 +35,16 @@ public class CacheClient
     private void postConstruct()
     {
         log.info("Creating gRPC channel and stub");
-        channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
-        cacheStub = CacheGrpc.newBlockingStub(channel);
+        try
+        {
+            channel = NettyChannelBuilder.forAddress(host, port)
+                    .sslContext(GrpcSslContexts.forClient().trustManager(new File(caCertificateName)).build()).build();
+            cacheStub = CacheGrpc.newBlockingStub(channel);
+        }
+        catch (SSLException e)
+        {
+            log.log(Level.SEVERE, e.getMessage(), e);
+        }
     }
 
     @PreDestroy
@@ -41,6 +56,17 @@ public class CacheClient
     public void setCache()
     {
         Item item = Item.newBuilder().setItemId(45).setItemName("Test Item").build();
-        ItemReply itemReply = cacheStub.setItem(item);
+        try
+        {
+            ItemReply itemReply = cacheStub.setItem(item);
+        }
+        catch (StatusRuntimeException e)
+        {
+            log.log(Level.SEVERE, e.getMessage(), e);
+        }
+        catch (Exception e)
+        {
+            log.log(Level.SEVERE, e.getMessage(), e);
+        }
     }
 }
